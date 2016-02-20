26ed6b8f6235de05f79435cd2f4fac7fe7385281
Michał Grygierzec <michal.grygierzec@gmail.com>
2016-02-20 13:46:29 +0100

    Connect suave web server to koding vm public IP address and default port

--- src/TimeMatrix.Web/app.fsx
+++ src/TimeMatrix.Web/app.fsx
@@ -4,4 +4,8 @@ open Suave                 // always open suave
 open Suave.Successful      // for OK-result
 open Suave.Web             // for config
 
-startWebServer defaultConfig (OK "Hello World!")
+let config = 
+    { defaultConfig with 
+        bindings = [ HttpBinding.mkSimple HTTP  "0.0.0.0" 80 ] }
+
+startWebServer config (OK "Hello World!")
