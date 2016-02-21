d32754c2fea7627641e299a012099e7b8cdd605d
Michał Grygierzec <michal.grygierzec@gmail.com>
2016-02-21 20:27:24 +0100

    Main page static html and assets

--- app.fsx
+++ app.fsx
@@ -1,7 +1,30 @@
 #r "packages/Suave/lib/net40/Suave.dll"
+#r "packages/DotLiquid/lib/NET40/DotLiquid.dll"
+#r "packages/Suave.DotLiquid/lib/net40/Suave.DotLiquid.dll"
 
+open System.IO
 open Suave                 // always open suave
 open Suave.Successful      // for OK-result
 open Suave.Web             // for config
+open Suave.Filters
+open Suave.Operators
 
-let app = OK "Hello F#!"
+DotLiquid.setTemplatesDir (__SOURCE_DIRECTORY__ + "/web/views")
+
+/// Browse static files in the 'web' subfolder
+let browseStaticFiles ctx = async {
+  let root = Path.Combine(ctx.runtime.homeDirectory, "web")
+  return! Files.browse root ctx }
+
+
+let homePage = DotLiquid.page "index.html" None
+
+let webPart = path "/" >=> homePage //request (fun r -> homePage)
+
+let home = OK "Hello F#!"
+
+let app =
+  choose [
+    webPart
+    browseStaticFiles
+  ]
--- build.fsx
+++ build.fsx
@@ -39,56 +39,58 @@ Target "Build" (fun _ ->
 )
 
 Target "Run" (fun _ ->
-  let config =
+  let prodConfig =
       { defaultConfig with
+          homeFolder = Some __SOURCE_DIRECTORY__
+          logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Debug
           bindings = [ HttpBinding.mkSimple HTTP  "0.0.0.0" 80 ] }
 
-  startWebServer config app
+  startWebServer prodConfig app
 )
-
-//Dev workflow
-let sbOut = new Text.StringBuilder()
-let sbErr = new Text.StringBuilder()
-
-let fsiSession =
-  let inStream = new StringReader("")
-  let outStream = new StringWriter(sbOut)
-  let errStream = new StringWriter(sbErr)
-  let fsiConfig = FsiEvaluationSession.GetDefaultConfiguration()
-  let argv = Array.append [|"/fake/fsi.exe"; "--quiet"; "--noninteractive"; "-d:DO_NOT_START_SERVER"|] [||]
-  FsiEvaluationSession.Create(fsiConfig, argv, inStream, outStream, errStream)
-
-let reportFsiError (e:exn) =
-  traceError "Reloading app.fsx script failed."
-  traceError (sprintf "Message: %s\nError: %s" e.Message (sbErr.ToString().Trim()))
-  sbErr.Clear() |> ignore
-
-let reloadScript () =
-  try
-    traceImportant "Reloading app.fsx script..."
-    let appFsx = __SOURCE_DIRECTORY__ @@ "app.fsx"
-    fsiSession.EvalInteraction(sprintf "#load @\"%s\"" appFsx)
-    fsiSession.EvalInteraction("open App")
-    match fsiSession.EvalExpression("app") with
-    | Some app -> Some(app.ReflectionValue :?> WebPart)
-    | None -> failwith "Couldn't get 'app' value"
-  with e -> reportFsiError e; None
-
-
-let currentApp = ref (fun _ -> async { return None })
-
-let serverConfig =
-  { defaultConfig with
-      homeFolder = Some __SOURCE_DIRECTORY__
-      logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Debug
-      bindings = [ HttpBinding.mkSimple HTTP "127.0.0.1" 8083] }
-
-let reloadAppServer () =
-  reloadScript() |> Option.iter (fun app ->
-    currentApp.Value <- app
-    traceImportant "New version of app.fsx loaded!" )
-
 Target "Dev" (fun _ ->
+
+  //Dev workflow
+  let sbOut = new Text.StringBuilder()
+  let sbErr = new Text.StringBuilder()
+
+  let fsiSession =
+    let inStream = new StringReader("")
+    let outStream = new StringWriter(sbOut)
+    let errStream = new StringWriter(sbErr)
+    let fsiConfig = FsiEvaluationSession.GetDefaultConfiguration()
+    let argv = Array.append [|"/fake/fsi.exe"; "--quiet"; "--noninteractive"; "-d:DO_NOT_START_SERVER"|] [||]
+    FsiEvaluationSession.Create(fsiConfig, argv, inStream, outStream, errStream)
+
+  let reportFsiError (e:exn) =
+    traceError "Reloading app.fsx script failed."
+    traceError (sprintf "Message: %s\nError: %s" e.Message (sbErr.ToString().Trim()))
+    sbErr.Clear() |> ignore
+
+  let reloadScript () =
+    try
+      traceImportant "Reloading app.fsx script..."
+      let appFsx = __SOURCE_DIRECTORY__ @@ "app.fsx"
+      fsiSession.EvalInteraction(sprintf "#load @\"%s\"" appFsx)
+      fsiSession.EvalInteraction("open App")
+      match fsiSession.EvalExpression("app") with
+      | Some app -> Some(app.ReflectionValue :?> WebPart)
+      | None -> failwith "Couldn't get 'app' value"
+    with e -> reportFsiError e; None
+
+
+  let currentApp = ref (fun _ -> async { return None })
+
+  let serverConfig =
+    { defaultConfig with
+        homeFolder = Some __SOURCE_DIRECTORY__
+        logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Debug
+        bindings = [ HttpBinding.mkSimple HTTP "127.0.0.1" 8083] }
+
+  let reloadAppServer () =
+    reloadScript() |> Option.iter (fun app ->
+      currentApp.Value <- app
+      traceImportant "New version of app.fsx loaded!" )
+
   let app ctx = currentApp.Value ctx
   let _, server = startWebServerAsync serverConfig app
 
