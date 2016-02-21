a9c51cbb1cb561f5aa454ae584088db7a7594c6e
Michał Grygierzec <michal.grygierzec@gmail.com>
2016-02-21 19:07:57 +0100

    Change project structure

--- webapp/app.fsx
+++ webapp/app.fsx
@@ -1,11 +1,11 @@
-#r "../packages/Suave/lib/net40/Suave.dll"
+#r "packages/Suave/lib/net40/Suave.dll"
 
 open Suave                 // always open suave
 open Suave.Successful      // for OK-result
 open Suave.Web             // for config
 
-let config = 
-    { defaultConfig with 
+let config =
+    { defaultConfig with
         bindings = [ HttpBinding.mkSimple HTTP  "0.0.0.0" 80 ] }
 
 startWebServer config (OK "Hello World!")
